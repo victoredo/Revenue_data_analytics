7ccad1861741a2a81403e73e01eedf806ecb2bd6
victoredo <okonvictor99@gmail.com>
2024-10-22 09:15:47 +0200

    Update non_modularised_mart_7_days_active_users.sql

--- models/mart/task2/non_modularised_mart_7_days_active_users.sql
+++ models/mart/task2/non_modularised_mart_7_days_active_users.sql
@@ -1,72 +1,23 @@
-WITH ranked_closures AS (
+WITH 
+
+open_accounts AS (
    SELECT
+       user_id_hashed,
        account_id_hashed,
+       created_ts,
        closed_ts,
-       ROW_NUMBER() OVER (
-           PARTITION BY account_id_hashed, CAST(closed_ts AS DATE)
-           ORDER BY closed_ts DESC
-       ) AS row_num
-   FROM `analytics-take-home-test.monzo_datawarehouse.account_closed`
-)
-
-, valid_closures AS (
-   SELECT
-       rc.account_id_hashed,
-       rc.closed_ts
-   FROM ranked_closures rc
-   WHERE rc.row_num = 1
-)
-
-, closure_reopen_pairs AS (
-   SELECT
-       ac.account_id_hashed,
-       ac.user_id_hashed,
-       ac.created_ts,
-       ac.account_type,
-       vc.closed_ts,
-       MIN(ro.reopened_ts) AS reopened_ts
-   FROM `analytics-take-home-test.monzo_datawarehouse.account_created` ac
-   LEFT JOIN valid_closures vc ON ac.account_id_hashed = vc.account_id_hashed
-   LEFT JOIN `analytics-take-home-test.monzo_datawarehouse.account_reopened` ro
-       ON ac.account_id_hashed = ro.account_id_hashed
-       AND ro.reopened_ts > vc.closed_ts
-   GROUP BY ac.account_id_hashed, ac.user_id_hashed, ac.created_ts, vc.closed_ts, ac.account_type
-)
-
-, account_lifecycle AS (
-   SELECT
-       cr.account_id_hashed,
-       cr.user_id_hashed,
-       cr.created_ts,
-       cr.closed_ts,
-       cr.reopened_ts,
-       cr.account_type,
-       CASE
-           WHEN cr.closed_ts IS NULL THEN 'open'
-           WHEN cr.reopened_ts IS NOT NULL THEN 'open'
-           ELSE 'closed'
-       END AS account_status
-   FROM closure_reopen_pairs cr
-)
-
-, open_accounts AS (
-   SELECT
-       al.user_id_hashed,
-       al.account_id_hashed,
-       al.created_ts,
-       al.closed_ts,
-       al.account_status
-   FROM account_lifecycle al
-   WHERE al.account_status = 'open'
-)
+       account_status
+   FROM analytics-take-home-test.victor_okon_take_home_task.unified_account_model
+   WHERE account_status in  ('open', 'reopened')
+),
 
 -- generate a list of unique dates (calculation dates)
-, unique_transaction_dates AS (
+unique_transaction_dates AS (
    SELECT DISTINCT CAST(tr.date AS DATE) AS calculation_date
    FROM `analytics-take-home-test.monzo_datawarehouse.account_transactions` tr
-)
+),
 
-, transactions_last_7_days AS (
+transactions_last_7_days AS (
    SELECT
        uad.calculation_date,
        oa.user_id_hashed,
@@ -81,9 +32,9 @@ WITH ranked_closures AS (
    LEFT JOIN `analytics-take-home-test.monzo_datawarehouse.account_transactions` tr
        ON oa.account_id_hashed = tr.account_id_hashed
    GROUP BY uad.calculation_date, oa.user_id_hashed
-)
+),
 
-, active_user_rate AS (
+active_user_rate AS (
    SELECT
        t.calculation_date,
        COUNT(DISTINCT CASE WHEN t.transactions_in_last_7_days > 0 THEN t.user_id_hashed END) AS active_users,
@@ -95,7 +46,8 @@ WITH ranked_closures AS (
 )
 
 SELECT *
-FROM  transactions_last_7_days
-WHERE user_id_hashed = 'CPhWQbkFSCK4nHlWNZYXnEKr5DpowKwUIDPeclqifhI8pE8enCy8CsAD3AYbgI/n/b6RNzPFraSB760fahkStg=='
---and calculation_date= '2020-07-05'
+FROM  active_user_rate
+order by calculation_date
+
+
 
